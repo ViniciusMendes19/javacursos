852930b3e63173487f7acf61777c173dd9ac9d53
Vinicius Mendes <vinicius.mendes1008@gmail.com>
2022-11-08 14:26:53 -0300

    exercicio caneta

--- entities/DadosAlunos.java
+++ entities/DadosAlunos.java
@@ -5,7 +5,7 @@ public class DadosAlunos {
     private String nome;
     private String email;
 
-    public DadosAlunos(){
+    public DadosAlunos() {
     }
 
     public DadosAlunos(String nome, String email) {
@@ -14,22 +14,28 @@ public class DadosAlunos {
     }
 
     public String getNome() {
+
         return nome;
     }
 
     public String getEmail() {
+
         return email;
     }
 
     public void setNome(String nome) {
+
         this.nome = nome;
     }
 
     public void setEmail(String email) {
+
         this.email = email;
     }
+
     // Para nao imprimir bugado
     public String toString() {
+
         return (nome + ", " + email);
     }
 }
